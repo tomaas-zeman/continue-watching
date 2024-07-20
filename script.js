a451859bfa029ef37607444d28d6ee223b37dd5c
Tomas Zeman <tomaas.zeman@gmail.com>
2024-07-20 21:47:47 +0200

    Support for Prime and SST + autoload in progress

--- script.js
+++ script.js
@@ -1,15 +1,56 @@
 chrome.action.onClicked.addListener((tab) => {
     chrome.scripting.executeScript({
         target: { tabId: tab.id },
-        function: scrollToContinueWatching
+        function: scrollToContinueWatching(tab),
     });
 });
 
-function scrollToContinueWatching() {
-    const xpath = `//*[text()='Continue watching']`;
-    const result = document.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null);
-    const element = result.singleNodeValue;
-    if (element) {
-        element.scrollIntoView({ behavior: 'smooth', block: 'start' });
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+    if (changeInfo.status === 'complete') {
+        chrome.scripting.executeScript({
+            target: { tabId },
+            files: ['script.js'],
+            function: moveContinueWatchingRow(tab),
+        });
+    }
+});
+
+function scrollToContinueWatching(tab) {
+    if (tab.url.includes("skyshowtime")) {
+        return function () {
+            const xpath = `//*[text()='Pokračovat ve sledování']`;
+            document
+                .evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null)
+                .singleNodeValue
+                .scrollIntoView({ behavior: 'smooth', block: 'start' });
+        }
+    } else if (tab.url.includes("primevideo")) {
+        return function () {
+            const xpath = `//*[text()='Continue watching']`;
+            document
+                .evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null)
+                .singleNodeValue
+                .scrollIntoView({ behavior: 'smooth', block: 'start' });
+        }
+    }
+}
+
+function moveContinueWatchingRow(tab) {
+    if (tab.url.includes("skyshowtime")) {
+        return function () {
+            const xpath = `//div[.//h2[text()='Pokračovat ve sledování'] and contains(@class, 'section-container__tray-item')]`;
+            const element = document
+                .evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null)
+                .singleNodeValue
+            element.parentNode.insertBefore(element, element.parentNode.firstElementChild);
+        }
+    } else if (tab.url.includes("primevideo")) {
+        return function () {
+            const xpath = `//div[.//p[text()='Continue watching'] and contains(@class, '+OSZzQ')]`;
+            const element = document
+                .evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null)
+                .singleNodeValue
+            element.parentNode.insertBefore(element, element.parentNode.firstElementChild);
+        }
     }
 }
